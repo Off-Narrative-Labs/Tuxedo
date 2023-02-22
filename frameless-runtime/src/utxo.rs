1fe20da147031655a447243253dfb9fb1c32c0e1
Andrew Burger <andrewburger1991@gmail.com>
2023-02-22 16:00:28 +0100

    Adding start of kitties piece impl

--- frameless-runtime/src/utxo.rs
+++ frameless-runtime/src/utxo.rs
@@ -282,10 +282,73 @@ impl TuxedoPiece for KittiesPiece {
     type Error = ();
 
     fn validate(transaction: Transaction) -> Result<(), Self::Error> {
-        // Check that the input is unique and a set
-        // if it fails then return early
+        PreValidator::<Self>::pre_validate(&transaction)?;
         // TODO: Implement Kitty Logic scenario
+
+        // 1.) If you want to breed Mom cannot have given birth before
+        // 2.) If you want to breed Dad cannot be too tired
+        for input in transaction.inputs.iter() {
+            let parents = PieceExtracter::<Self>::extract(input.output)?;
+            Self::mom_and_dad_ready(&parents)?;
+        }
         Ok(())
+    }
+}
+
+impl UtxoSet for KittiesPiece {
+    /// Check whether a given utxo exists in the current set
+    fn contains(utxo_ref: UtxoRef) -> bool {
+        sp_io::storage::exists(&utxo_ref.encode())
+    }
+
+    /// Insert the given utxo into the state storing it with the given ref
+    /// The ref is probably the hash of a tx that created it and its index in that tx, but this decision is opaque to this trait
+    /// Return whether the operation is successful (It can fail if the ref is already present)
+    fn insert(utxo_ref: UtxoRef, utxo: &Utxo) -> bool {
+        sp_io::storage::set(&utxo_ref.encode(), &utxo.encode());
+        true
+    }
+
+    ///
+    /// nullify the utxo by either:
+    /// - Consuming it entirely
+    /// - Putting it on "Timeout"
+    /// - Not consuming it but marking it as spent
+    ///
+    fn nullify(utxo_ref: UtxoRef) -> Option<Utxo> {
+        None
+        // let parents = PieceExtractor::<Self>::extract(utxo_ref).ok_or(())?;
+        // if Self::mom_ready(&parents.mom) {
+
+        // }
+        // let encoded_utxo = sp_io::storage::get(&utxo_ref.encode())?;
+        // match Utxo::decode(&mut &encoded_utxo[..]) {
+        //     Ok(utxo) => Some(utxo),
+        //     Err(_) => None,
+        // }
+        // PieceExtractor::<Self>::extract(utxo_ref)
+    }
+}
+
+impl KittiesPiece {
+    fn mom_and_dad_ready(parents: &<Self as TuxedoPiece>::Data) -> Result<(), ()> {
+        Self::mom_ready(&parents.mom).then_some(()).ok_or(())?;
+        Self::dad_ready(&parents.dad).then_some(()).ok_or(())?;
+        Ok(())
+    }
+
+    fn mom_ready(mom_status: &MomKittyStatus) -> bool {
+        match mom_status {
+            MomKittyStatus::RearinToGo => true,
+            MomKittyStatus::HadBirthRecently => false,
+        }
+    }
+
+    fn dad_ready(dad_status: &DadKittyStatus) -> bool {
+        match dad_status {
+            DadKittyStatus::RearinToGo => true,
+            DadKittyStatus::Tired => false,
+        }
     }
 }
 
