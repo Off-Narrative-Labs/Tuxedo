7cbfe6713670f38d9319bc3b31066c2e1ac7f261
Joshy Orndorff <admin@joshyorndorff.com>
2023-03-03 17:27:10 -0500

    rebrand first attempt

--- node/src/service.rs
+++ node/src/service.rs
@@ -16,11 +16,6 @@ use crate::rpc;
 pub struct ExecutorDispatch;
 
 impl sc_executor::NativeExecutionDispatch for ExecutorDispatch {
-	/// Only enable the benchmarking host functions when we actually want to benchmark.
-	#[cfg(feature = "runtime-benchmarks")]
-	type ExtendHostFunctions = frame_benchmarking::benchmarking::HostFunctions;
-	/// Otherwise we only use the default Substrate host functions.
-	#[cfg(not(feature = "runtime-benchmarks"))]
 	type ExtendHostFunctions = ();
 
 	fn dispatch(method: &str, data: &[u8]) -> Option<Vec<u8>> {
