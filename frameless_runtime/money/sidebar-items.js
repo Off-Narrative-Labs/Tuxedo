bb6cd68f8106a84e386213a7d0dbeb952d162f94
JoshOrndorff <JoshOrndorff@users.noreply.github.com>
2023-03-03 04:54:28 +0000

    deploy: 586e2a73e6fa36a4e07b060d896f7fd684645e36

---
+++ frameless_runtime/money/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["MoneyVerifier","VerifierError"],"struct":["Coin"]};
--- implementors/tuxedo_core/verifier/trait.Verifier.js
+++ implementors/tuxedo_core/verifier/trait.Verifier.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
-"frameless_runtime":[["impl <a class=\"trait\" href=\"tuxedo_core/verifier/trait.Verifier.html\" title=\"trait tuxedo_core::verifier::Verifier\">Verifier</a> for <a class=\"enum\" href=\"frameless_runtime/enum.OuterVerifier.html\" title=\"enum frameless_runtime::OuterVerifier\">OuterVerifier</a>"]],
+"frameless_runtime":[["impl <a class=\"trait\" href=\"tuxedo_core/verifier/trait.Verifier.html\" title=\"trait tuxedo_core::verifier::Verifier\">Verifier</a> for <a class=\"enum\" href=\"frameless_runtime/enum.OuterVerifier.html\" title=\"enum frameless_runtime::OuterVerifier\">OuterVerifier</a>"],["impl <a class=\"trait\" href=\"tuxedo_core/verifier/trait.Verifier.html\" title=\"trait tuxedo_core::verifier::Verifier\">Verifier</a> for <a class=\"enum\" href=\"frameless_runtime/money/enum.MoneyVerifier.html\" title=\"enum frameless_runtime::money::MoneyVerifier\">MoneyVerifier</a>"]],
 "tuxedo_core":[]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
