602002f0845abfaef12bddb2c913d7e2d81baf47
Andrew Burger <andrewburger1991@gmail.com>
2023-03-08 15:31:53 +0100

    Adding explanation plus fixing bug

--- tuxedo-template-runtime/src/kitties.rs
+++ tuxedo-template-runtime/src/kitties.rs
@@ -1,3 +1,21 @@
+//! An NFT game inspired by cryptokitties.
+//! This is a game which allows for kitties to be bred based on a few factors
+//! 1.) Mom and Tired have to be in a state where they are ready to breed
+//! 2.) Each Mom and Dad have some DNA and the child will have unique DNA combined from the both of them
+//!     Linkable back to the Mom and Dad
+//! 3.) The game also allows Kitties to have a cooling off period inbetween breeding before they can be bred again.
+//! 4.) A rest operation allows for a Mom Kitty and a Dad Kitty to be cooled off
+//!
+//! In order to submit a valid transaction you must strutucture it as follows:
+//! 1.) Input must contain 1 mom and 1 dad
+//! 2.) Output must contain Mom, Dad, and newly created Child
+//! 3.) A child's DNA is calculated by:
+//!         BlakeTwo256::hash_of(MomDna, DadDna, MomCurrNumBreedings, DadCurrNumberBreedings)
+//!
+//! There are a only a finite amount of free breedings available before it starts to cost money
+//! to breed kitties.
+//!
+
 use parity_scale_codec::{Decode, Encode};
 use scale_info::TypeInfo;
 #[cfg(feature = "std")]
@@ -221,7 +239,7 @@ impl Breed for KittyHelpers {
     fn check_free_breedings(mom: &KittyData, dad: &KittyData) -> Result<(), Self::Error> {
         let mom_breedings = mom.free_breedings;
         let dad_breedings = dad.free_breedings;
-        if (mom_breedings == 0) && (dad_breedings == 0) {
+        if (mom_breedings == 0) || (dad_breedings == 0) {
             return Err(Self::Error::NotEnoughFreeBreedings);
         }
         Ok(())
