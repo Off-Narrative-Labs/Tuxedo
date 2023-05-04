15d2f8124d32445349189524d7af5d59edcba022
Joshy Orndorff <admin@joshyorndorff.com>
2023-05-04 15:55:31 -0400

    fix warnings in tests

--- tuxedo-template-runtime/tests/add_token.rs
+++ tuxedo-template-runtime/tests/add_token.rs
@@ -2,7 +2,7 @@ use tuxedo_template_runtime::OuterConstraintChecker::{self, *};
 
 #[test]
 fn has_three_variants() {
-    fn match_outer_constraint_checker(c: OuterConstraintChecker) {
+    fn _match_outer_constraint_checker(c: OuterConstraintChecker) {
         match c {
             Money(_) => (),
             RuntimeUpgrade(_) => (),
@@ -14,5 +14,5 @@ fn has_three_variants() {
 #[test]
 fn second_token_is_id_1() {
     let coin_1_checker = money::MoneyConstraintChecker::<1>::Mint;
-    let outer_checker = SecondToken(coin_1_checker);
+    let _outer_checker = SecondToken(coin_1_checker);
 }
