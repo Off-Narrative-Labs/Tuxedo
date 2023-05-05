c0e49b86511cbc1a7efa9bb1c8105f7504d73074
Joshy Orndorff <admin@joshyorndorff.com>
2023-05-05 14:16:26 -0400

    bring in derive no bound from substrate

--- tuxedo-core/src/support_macros.rs
+++ tuxedo-core/src/support_macros.rs
@@ -1,6 +1,9 @@
 //! These macros are copied from frame-support. Substrate maintainers are not open to putting them in
 //! a more sensible location. See https://github.com/paritytech/substrate/issues/13456
 
+// Bring in the no_bound ones that are in another crate because they are proc macros
+pub use derive_no_bound::{CloneNoBound, DebugNoBound, DefaultNoBound};
+
 /// Return Err of the expression: `return Err($expression);`.
 ///
 /// Used as `fail!(expression)`.
