4280617484ef66685855d1abe2fe5c05cf7cc9ad
JoshOrndorff <JoshOrndorff@users.noreply.github.com>
2023-09-26 17:47:32 +0000

    deploy: 45734bf018bfbc9ca8bc5e11fd41945afb39ea22

--- implementors/sp_runtime/traits/trait.Extrinsic.js
+++ implementors/sp_runtime/traits/trait.Extrinsic.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"tuxedo_core":[["impl&lt;V, C&gt; Extrinsic for <a class=\"struct\" href=\"tuxedo_core/types/struct.Transaction.html\" title=\"struct tuxedo_core::types::Transaction\">Transaction</a>&lt;V, C&gt;"]]
+"tuxedo_core":[["impl&lt;V: TypeInfo + 'static, C: TypeInfo + 'static&gt; Extrinsic for <a class=\"struct\" href=\"tuxedo_core/types/struct.Transaction.html\" title=\"struct tuxedo_core::types::Transaction\">Transaction</a>&lt;V, C&gt;"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
