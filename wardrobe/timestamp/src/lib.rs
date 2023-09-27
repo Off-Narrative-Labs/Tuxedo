771d02e0ce3dcb68c8b6567f7209c4693b4e0b11
Joshy Orndorff <git-user-email.h0ly5@simplelogin.com>
2023-09-27 08:28:55 -0400

    Sketch piece draft

---
+++ wardrobe/timestamp/src/lib.rs
@@ -0,0 +1,116 @@
+//! Allow block authors to include a timestamp via an inherent transaction
+//! 
+//! This strives to be roughly analogous to FRAME's pallet timestamp and uses the same client-side inherent data provider logic.
+//! 
+//! In this first iteration, block authors set timestamp once per block by adding a new utxo. The timestamps are never cleaned up. There are no incentives.
+//! In the future, it may make sense to have them consume the previous timestamp or a timestamp from n blocks ago, or just provide incentives for users to clean up old ones.
+//! 
+//! Some things that are still a little unclear. How do we make sure that this function is only called via inherent? Maybe forbid it in the pool.
+//! If forbidding them in the pool is the answer, then we need to  consider a way to make it easy / nice for piece developers to declare that their transactions are inherents.
+//! One way to make it work for users would be to adapt the macro so that you can add an `#[inherent]` to some call variants, then the macro creates a function called is_inherent that checks if it is an inherent or not.
+
+#![cfg_attr(not(feature = "std"), no_std)]
+
+use parity_scale_codec::{Decode, Encode};
+use scale_info::TypeInfo;
+#[cfg(feature = "std")]
+use serde::{Deserialize, Serialize};
+use sp_runtime::transaction_validity::TransactionPriority;
+use tuxedo_core::{
+    dynamic_typing::{DynamicallyTypedData, UtxoData},
+    ensure, SimpleConstraintChecker,
+};
+use sp_timestamp::Timestamp;
+
+#[cfg(test)]
+mod tests;
+
+/// A wrapper around a u64 that holds the Unix epoch time in milliseconds.
+/// Basically the same as sp_timestamp::Timestamp, but we need this type
+/// to implement the UtxoData trait since they are both foreign.
+#[derive(Debug, Encode, Decode, Eq, Clone, Copy, Default, Ord)]
+struct StorableTimestamp(u64);
+
+impl From<Timestamp> for StorableTimestamp {
+    //todo
+}
+
+impl From<StorableTimestamp> for Timestamp {
+    //todo
+}
+
+impl UtxoData for StorableTimestamp {
+    const TYPE_ID: [u8; 4] = *b"time";
+}
+
+/// Reasons that setting or reading the timestamp may go wrong.
+#[derive(Debug, Eq, PartialEq)]
+pub enum TimestampError {
+    /// The timetamp may not eb set more than once in a block, but this block attempts to do so.
+    TimestampAlreadySet,
+
+    // TODO I'm getting tired of checking the right number of inputs and outputs in every single piece, maybe we should ahve some helpers for this common task.
+    // Like it expects exactly N inputs (or outputs) and you give it an error for when there are too many and another for when there are too few.
+
+    /// UTXO data has an unexpected type
+    BadlyTypedOutput,
+    /// No outputs were specified when setting the timestamp, but exactly one is required.
+    MissingTimestamp,
+    /// Multiple outputs were specified while setting the timestamp, but exactly one is required.
+    TooManyOutputsWhileSettingTimestamp,
+    /// No inputs are expected when setting the timestamp. But this transaction specified one.
+    UnexpectedInputWhileSettingTimestamp,
+}
+
+/// A constraint checker for the simple act of setting the timetamp.
+///
+/// This is expected to be performed through an inherent, and to happen exactly once per block.
+/// The earlier it happens in the block the better, and concretely we expect authoring nodes to
+/// insert this information first via an inehrent extrinsic.
+#[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
+#[derive(Encode, Decode, Debug, PartialEq, Eq, Clone, TypeInfo)]
+pub struct SetTimestamp;
+
+impl SimpleConstraintChecker for AmoebaCreation {
+    type Error = TimestampError;
+
+    fn check(
+        &self,
+        input_data: &[DynamicallyTypedData],
+        _peeks: &[DynamicallyTypedData],
+        output_data: &[DynamicallyTypedData],
+    ) -> Result<TransactionPriority, Self::Error> {
+        // In FRAME we use ensure_none! to make sure this is an inherent (no origin means inherent).
+        // The implementation is easy in FRAME where nearly every transaction is signed.
+        // However in the UTXO model, no transactions are signed in their entirety, so there is no simple way to
+        // tell that this is an inherent at this point. My plan to address this (at the moment) is to match on
+        // the call type in the pool, and not propagate ones that were marked as inherents.
+
+        // Make sure there is a single output of the correct type
+        ensure!(
+            !output_data.is_empty(),
+            Self::Error::MissingTimestamp
+        );
+        ensure!(
+            output_data.len() == 1,
+            Self::Error::TooManyOutputsWhileSettingTimestamp
+        );
+        let millis_since_epoch = output_data[0]
+            .extract::<StorableTimestamp>()
+            .map_err(|_| Self::Error::BadlyTypedOutput)?
+            .0;
+
+        // TODO We really need to make sure that the new timestamp is greater than the previous high water mark.
+        // Im not handling this right now. It will require more inputs and outputs. I just want to get something basic working right meow.
+
+        // Make sure there are no inputs.
+        // We may require cleaning up an old timestamp in the future, or may provide incentives for anyone to do that.
+        // For now they stick around as storage bloat.
+        ensure!(
+            input_data.is_empty(),
+            Self::Error::UnexpectedInputWhileSettingTimestamp
+        );
+
+        Ok(0)
+    }
+}
---
+++ wardrobe/timestamp/src/tests.rs
@@ -0,0 +1,302 @@
+//! Unit tests for the Amoeba piece
+
+use super::*;
+use tuxedo_core::dynamic_typing::testing::Bogus;
+
+#[test]
+fn creation_valid_transaction_works() {
+    let to_spawn = AmoebaDetails {
+        generation: 0,
+        four_bytes: *b"test",
+    };
+    let input_data = Vec::new();
+    let output_data = vec![to_spawn.into()];
+
+    assert_eq!(AmoebaCreation.check(&input_data, &[], &output_data), Ok(0),);
+}
+
+#[test]
+fn creation_invalid_generation_fails() {
+    let to_spawn = AmoebaDetails {
+        generation: 100,
+        four_bytes: *b"test",
+    };
+    let input_data = Vec::new();
+    let output_data = vec![to_spawn.into()];
+
+    assert_eq!(
+        AmoebaCreation.check(&input_data, &[], &output_data),
+        Err(ConstraintCheckerError::WrongGeneration),
+    );
+}
+
+#[test]
+fn creation_with_inputs_fails() {
+    let example = AmoebaDetails {
+        generation: 0,
+        four_bytes: *b"test",
+    };
+    let input_data = vec![example.clone().into()];
+    let output_data = vec![example.into()];
+
+    assert_eq!(
+        AmoebaCreation.check(&input_data, &[], &output_data),
+        Err(ConstraintCheckerError::CreationMayNotConsume),
+    );
+}
+
+#[test]
+fn creation_with_badly_typed_output_fails() {
+    let input_data = Vec::new();
+    let output_data = vec![Bogus.into()];
+
+    assert_eq!(
+        AmoebaCreation.check(&input_data, &[], &output_data),
+        Err(ConstraintCheckerError::BadlyTypedOutput),
+    );
+}
+
+#[test]
+fn creation_multiple_fails() {
+    let to_spawn = AmoebaDetails {
+        generation: 0,
+        four_bytes: *b"test",
+    };
+    let input_data = Vec::new();
+    let output_data = vec![to_spawn.clone().into(), to_spawn.into()];
+
+    assert_eq!(
+        AmoebaCreation.check(&input_data, &[], &output_data),
+        Err(ConstraintCheckerError::CreatedTooMany),
+    );
+}
+
+#[test]
+fn creation_with_no_output_fails() {
+    let input_data = Vec::new();
+    let output_data = Vec::new();
+
+    assert_eq!(
+        AmoebaCreation.check(&input_data, &[], &output_data),
+        Err(ConstraintCheckerError::CreatedNothing),
+    );
+}
+
+#[test]
+fn mitosis_valid_transaction_works() {
+    let mother = AmoebaDetails {
+        generation: 1,
+        four_bytes: *b"test",
+    };
+    let d1 = AmoebaDetails {
+        generation: 2,
+        four_bytes: *b"test",
+    };
+    let d2 = AmoebaDetails {
+        generation: 2,
+        four_bytes: *b"test",
+    };
+    let input_data = vec![mother.into()];
+    let output_data = vec![d1.into(), d2.into()];
+
+    assert_eq!(AmoebaMitosis.check(&input_data, &[], &output_data), Ok(0),);
+}
+
+#[test]
+fn mitosis_wrong_generation() {
+    let mother = AmoebaDetails {
+        generation: 1,
+        four_bytes: *b"test",
+    };
+    let d1 = AmoebaDetails {
+        generation: 3, // This daughter has the wrong generation
+        four_bytes: *b"test",
+    };
+    let d2 = AmoebaDetails {
+        generation: 2,
+        four_bytes: *b"test",
+    };
+    let input_data = vec![mother.into()];
+    let output_data = vec![d1.into(), d2.into()];
+
+    assert_eq!(
+        AmoebaMitosis.check(&input_data, &[], &output_data),
+        Err(ConstraintCheckerError::WrongGeneration),
+    );
+}
+
+#[test]
+fn mitosis_badly_typed_input() {
+    let mother = Bogus;
+    let d1 = AmoebaDetails {
+        generation: 2,
+        four_bytes: *b"test",
+    };
+    let d2 = AmoebaDetails {
+        generation: 2,
+        four_bytes: *b"test",
+    };
+    let input_data = vec![mother.into()];
+    let output_data = vec![d1.into(), d2.into()];
+
+    assert_eq!(
+        AmoebaMitosis.check(&input_data, &[], &output_data),
+        Err(ConstraintCheckerError::BadlyTypedInput),
+    );
+}
+
+#[test]
+fn mitosis_no_input() {
+    let d1 = AmoebaDetails {
+        generation: 2,
+        four_bytes: *b"test",
+    };
+    let d2 = AmoebaDetails {
+        generation: 2,
+        four_bytes: *b"test",
+    };
+    let input_data = Vec::new();
+    let output_data = vec![d1.into(), d2.into()];
+
+    assert_eq!(
+        AmoebaMitosis.check(&input_data, &[], &output_data),
+        Err(ConstraintCheckerError::WrongNumberOfMothers),
+    );
+}
+
+#[test]
+fn mitosis_badly_typed_output() {
+    let mother = AmoebaDetails {
+        generation: 1,
+        four_bytes: *b"test",
+    };
+    let d1 = AmoebaDetails {
+        generation: 2,
+        four_bytes: *b"test",
+    };
+    let d2 = Bogus;
+    let input_data = vec![mother.into()];
+    let output_data = vec![d1.into(), d2.into()];
+
+    assert_eq!(
+        AmoebaMitosis.check(&input_data, &[], &output_data),
+        Err(ConstraintCheckerError::BadlyTypedOutput),
+    );
+}
+
+#[test]
+fn mitosis_only_one_output() {
+    let mother = AmoebaDetails {
+        generation: 1,
+        four_bytes: *b"test",
+    };
+    let d1 = AmoebaDetails {
+        generation: 2,
+        four_bytes: *b"test",
+    };
+    let input_data = vec![mother.into()];
+    // There is only one daughter when there should be two
+    let output_data = vec![d1.into()];
+
+    assert_eq!(
+        AmoebaMitosis.check(&input_data, &[], &output_data),
+        Err(ConstraintCheckerError::WrongNumberOfDaughters),
+    );
+}
+
+#[test]
+fn mitosis_too_many_outputs() {
+    let mother = AmoebaDetails {
+        generation: 1,
+        four_bytes: *b"test",
+    };
+    let d1 = AmoebaDetails {
+        generation: 2,
+        four_bytes: *b"test",
+    };
+    let d2 = AmoebaDetails {
+        generation: 2,
+        four_bytes: *b"test",
+    };
+    // Mitosis requires exactly two daughters. There should not be a third one.
+    let d3 = AmoebaDetails {
+        generation: 2,
+        four_bytes: *b"test",
+    };
+    let input_data = vec![mother.into()];
+    let output_data = vec![d1.into(), d2.into(), d3.into()];
+
+    assert_eq!(
+        AmoebaMitosis.check(&input_data, &[], &output_data),
+        Err(ConstraintCheckerError::WrongNumberOfDaughters),
+    );
+}
+
+#[test]
+fn death_valid_transaction_works() {
+    let example = AmoebaDetails {
+        generation: 1,
+        four_bytes: *b"test",
+    };
+    let input_data = vec![example.into()];
+    let output_data = vec![];
+
+    assert_eq!(AmoebaDeath.check(&input_data, &[], &output_data), Ok(0),);
+}
+
+#[test]
+fn death_no_input() {
+    let input_data = vec![];
+    let output_data = vec![];
+
+    assert_eq!(
+        AmoebaDeath.check(&input_data, &[], &output_data),
+        Err(ConstraintCheckerError::NoVictim),
+    );
+}
+
+#[test]
+fn death_multiple_inputs() {
+    let a1 = AmoebaDetails {
+        generation: 1,
+        four_bytes: *b"test",
+    };
+    let a2 = AmoebaDetails {
+        generation: 4,
+        four_bytes: *b"test",
+    };
+    let input_data = vec![a1.into(), a2.into()];
+    let output_data = vec![];
+
+    assert_eq!(
+        AmoebaDeath.check(&input_data, &[], &output_data),
+        Err(ConstraintCheckerError::TooManyVictims),
+    );
+}
+
+#[test]
+fn death_with_output() {
+    let example = AmoebaDetails {
+        generation: 1,
+        four_bytes: *b"test",
+    };
+    let input_data = vec![example.clone().into()];
+    let output_data = vec![example.into()];
+
+    assert_eq!(
+        AmoebaDeath.check(&input_data, &[], &output_data),
+        Err(ConstraintCheckerError::DeathMayNotCreate),
+    );
+}
+
+#[test]
+fn death_badly_typed_input() {
+    let example = Bogus;
+    let input_data = vec![example.into()];
+    let output_data = vec![];
+
+    assert_eq!(
+        AmoebaDeath.check(&input_data, &[], &output_data),
+        Err(ConstraintCheckerError::BadlyTypedInput),
+    );
+}
