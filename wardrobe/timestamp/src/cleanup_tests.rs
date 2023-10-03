8ed0943153543bd158372d012c0b1e744b3a3cd9
Joshy Orndorff <git-user-email.h0ly5@simplelogin.com>
2023-10-03 12:07:28 -0400

    moar cleanup tests

--- wardrobe/timestamp/src/cleanup_tests.rs
+++ wardrobe/timestamp/src/cleanup_tests.rs
@@ -16,12 +16,65 @@ fn cleanup_timestamp_happy_path() {
     assert_eq!(CleanUpTimestamp.check(&input_data, &peek_data, &[]), Ok(0),);
 }
 
-// Cleanup happy case
-// Cleanup input is best, not noted
-// Cleanup, input is newer than reference
-// Cleanup input is older than reference, but not by enough
-// Cleanup missing input
-// Cleanup missing reference
+#[test]
+fn cleanup_timestamp_input_is_best_not_noted() {
+    let old = BestTimestamp(100);
+    let newer = NotedTimestamp(2 * CLEANUP_AGE);
+
+    let input_data = vec![old.into()];
+    let peek_data = vec![newer.into()];
+
+    assert_eq!(CleanUpTimestamp.check(&input_data, &peek_data, &[]), Err(BadlyTyped));
+}
+
+#[test]
+fn cleanup_timestamp_input_newer_than_reference() {
+    let old = NotedTimestamp(200);
+    let newer = NotedTimestamp(100);
+
+    let input_data = vec![old.into()];
+    let peek_data = vec![newer.into()];
+
+    assert_eq!(CleanUpTimestamp.check(&input_data, &peek_data, &[]), Err(DontBeSoHasty));
+}
+
+#[test]
+fn cleanup_timestamp_input_not_yet_ripe_for_cleaning() {
+    let old = NotedTimestamp(200);
+    let newer = NotedTimestamp(300);
+
+    let input_data = vec![old.into()];
+    let peek_data = vec![newer.into()];
+
+    assert_eq!(CleanUpTimestamp.check(&input_data, &peek_data, &[]), Err(DontBeSoHasty));
+}
+
+#[test]
+fn cleanup_timestamp_missing_reference() {
+    let old = NotedTimestamp(200);
+
+    let input_data = vec![old.into()];
+    let peek_data = vec![];
+
+    assert_eq!(CleanUpTimestamp.check(&input_data, &peek_data, &[]), Err(CleanupRequiresOneReference));
+}
+
+#[test]
+fn cleanup_timestamp_missing_input() {
+    // 
+
+    let newer = NotedTimestamp(100);
+
+    let input_data = vec![];
+    let peek_data = vec![newer.into()];
+
+    assert_eq!(CleanUpTimestamp.check(&input_data, &peek_data, &[]), Ok(0));
+}
+
+// cleanup multiple happy path
+
+// cleanup multiple, first is valid, second is not
+
 // Cleanup input is wrong type
 // Reference is wrong type
 // Cleanup output is wrong type
