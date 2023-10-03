2d395a056184b7ec9f7e239db1bdcb2f15b822a6
Joshy Orndorff <git-user-email.h0ly5@simplelogin.com>
2023-10-03 12:27:23 -0400

    test first block hack

--- wardrobe/timestamp/src/first_block_special_case_tests.rs
+++ wardrobe/timestamp/src/first_block_special_case_tests.rs
@@ -3,13 +3,33 @@
 //! without consuming any previous one. I hope to remove this hack by including a timestamp extrinsic
 //! in the genesis block. I've asked for some background about that in
 //! https://substrate.stackexchange.com/questions/10105/extrinsics-in-genesis-block
+//! And also sketched a path toward a timestamp in the genesis block in
+//! https://github.com/Off-Narrative-Labs/Tuxedo/issues/107
 
 use super::*;
 use tuxedo_core::dynamic_typing::testing::Bogus;
 
-// Set timestamp happy case
-// Set timestamp no outputs
-// Set timestamp no new best
-// Set timestamp no new noted
-// Set timestamp noted not equal to best
-// Set timestamp too many outputs
+use TimestampError::*;
+
+
+/// The mock config always says the block number is one.
+pub struct AlwaysBlockOne;
+
+impl TimestampConfig for AlwaysBlockOne {
+    fn block_height() -> u32 {
+        1
+    }
+}
+
+#[test]
+fn set_timestamp_first_block_happy_path() {
+    let checker = SetTimestamp::<AlwaysBlockOne>(Default::default());
+
+    let new_best = BestTimestamp(400);
+    let new_noted = NotedTimestamp(400);
+
+    let input_data = vec![];
+    let output_data = vec![new_best.into(), new_noted.into()];
+
+    assert_eq!(checker.check(&input_data, &[], &output_data), Ok(0),);
+}
