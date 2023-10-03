8ecdba2df3e96cf238d59e2f9f5c7254d97ab456
Joshy Orndorff <git-user-email.h0ly5@simplelogin.com>
2023-10-03 12:00:45 -0400

    first cleanup test

--- wardrobe/timestamp/src/cleanup_tests.rs
+++ wardrobe/timestamp/src/cleanup_tests.rs
@@ -3,6 +3,18 @@
 
 use super::*;
 use tuxedo_core::dynamic_typing::testing::Bogus;
+use TimestampError::*;
+
+#[test]
+fn cleanup_timestamp_happy_path() {
+    let old = NotedTimestamp(100);
+    let newer = NotedTimestamp(2 * CLEANUP_AGE);
+
+    let input_data = vec![old.into()];
+    let peek_data = vec![newer.into()];
+
+    assert_eq!(CleanUpTimestamp.check(&input_data, &peek_data, &[]), Ok(0),);
+}
 
 // Cleanup happy case
 // Cleanup input is best, not noted
@@ -11,4 +23,5 @@ use tuxedo_core::dynamic_typing::testing::Bogus;
 // Cleanup missing input
 // Cleanup missing reference
 // Cleanup input is wrong type
+// Reference is wrong type
 // Cleanup output is wrong type
