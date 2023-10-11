d8220ab8dea4fd325342b23819811b469471d7cf
Joshy Orndorff <git-user-email.h0ly5@simplelogin.com>
2023-10-11 16:24:13 -0400

    revise inherents file

--- tuxedo-core/src/inherents.rs
+++ tuxedo-core/src/inherents.rs
@@ -1,46 +1,36 @@
-//! Utilities for working with inherents in Tuxedo based chains
+//! APIs and utilities for working with Substrate's Inherents in Tuxedo based chains.
 //!
-//! # Background on inherents
+//! # Substrate inherents
 //!
-//! Inherents are a Substrate feature that allows block authors to insert some data from the environment
-//! into the body of the block. Inherents are similar to pre-runtime digests which are persisted in the block header,
-//! but they differ because inherents go into the block body.
+//! Inherents are a Substrate feature that allows block authors to insert some transactions directly
+//! into the body of the block. Inherents are similar to pre-runtime digests which allow authors to
+//! insert info into the block header. However inherents go in the block body and therefore must be transactions.
 //!
-//! Some classic usecases are a block timestamp, information about the relay chain (if the current chain is a parachain),
+//! Classic usecases for inherents are injecting and updating environmental information such as a block timestamp,
+//! information about the relay chain (if the current chain is a parachain),
 //! or information about who should receive the block reward.
+//! 
+//! In order to allow the runtime to construct such transactions while keeping the cleint opaque, there are
+//! special APIs for creating inherents and performing off-chain validation of inherents. That's right,
+//! inherents also offer a special API to have their environmental data checked off-chain before the block
+//! is executed.
 //!
 //! # Complexities in UTXO chains
 //!
-//! In account based systems, the classic way to use an inherent is that the block author calls the runtime providing the current timestamp.
-//! The runtime returns an extrinsic that will set the timestamp on-chain. The block author includes that inherent extrinsic in the block author.
-//! The purpose for the extra round-trip to the runtime is to facilitate runtime upgrades, I believe. Then when the extrinsic executes it, for example,
-//! overwrites the timestamp in a dedicated storage item.
+//! In account based systems, the classic way to use an inherent is that the block inserts a transaction providing some data like a timestamp.
+//! When the extrinsic executed it, overwrites the previously stored timestamp in a dedicated storage item.
 //!
 //! In UTXO chains, there are no storage items, and all state is local to a UTXO. This is the case with, for example, the timestamp as well.
 //! This means that when the author calls into the runtime with a timestamp, the transaction that is returned must include the correct reference
-//! to the UTXO that contained the previous best timestamp. This is the crux of the problem. There is no easy way to know the location of
+//! to the UTXO that contained the previous best timestamp. This is the crux of the problem: there is no easy way to know the location of
 //! the previous timestamp in the utxo-space from inside the runtime.
 //!
 //! # Scraping the Parent Block
 //!
 //! The solution is to provide the entirety of the previous block to the runtime when asking it to construct inherents.
-//! This module provides an inherent data provider that does just this. Any Tuxedo runtime that uses inherents (At least ones,
-//! like I've described above), they need to first include this foundational inherent data provider that provvides the previous
-//! block so that the Tuxedo executive can scrape it to find the output references of the previous inherent transactions.
-//!
-//! # Inherent-Per-Block Cadence Assumption
-//!
-//! This entire process assumes that the previous state that needs to be consumed exists in the immediate parent block.
-//! This is only guaranteed if the inherent is included in every single block. Currently all the usecases I mentioned above
-//! meet this assumption, and this seems to be the way that inehrents are used in general.
-//!
-//! If we find that there are compelling usecases for a more flexible cadence, some options to explore include:
-//! 1. Use a custom authoring trait which we are already considering for end-of-block inherents anyway.
-//! 2. Use the auxiliary storage to keep track of the utxo refs on a block-by-block basis
-//!
-//! I'm starting to think that inherents are always going to be tied to block cadence. If this is not the case,
-//! I would argue that the task of inserting data should not be tied to block authorship, and instead left to some kind of on-chain
-//! game or dao that anyone can participate in.
+//! This module provides an inherent data provider that does just this. Any Tuxedo runtime that uses inherents (At least ones
+//! that update environmental data), needs to include this foundational previous block inherent data provider that provvides
+//! so that the Tuxedo executive can scrape it to find the output references of the previous inherent transactions.
 
 use parity_scale_codec::{Decode, Encode};
 use scale_info::TypeInfo;
@@ -58,6 +48,7 @@ pub const PARENT_INHERENT_IDENTIFIER: InherentIdentifier = *b"prnt_blk";
 
 /// An inherent data provider that inserts the previous block into the inherent data.
 /// This data does NOT go into an extrinsic.
+#[cfg(feature = "std")]
 pub struct ParentBlockInherentDataProvider<Block>(pub Block);
 
 #[cfg(feature = "std")]
@@ -94,13 +85,16 @@ impl<B: BlockT> sp_inherents::InherentDataProvider for ParentBlockInherentDataPr
     }
 }
 
-/// Tuxedo's interface around Substrate's concept of inherents.
+/// Tuxedo's controlled interface around Substrate's concept of inherents.
 ///
-/// Tuxedo assumes that each inherent will appear exactly once in each block.
-/// It is recommended that inherent constraint checkers use their Accumulator to verify this
-/// at the end of each block.
+/// This interface assumes that each inherent will appear exactly once in each block.
+/// This will be verified off-chain by nodes before block execution begins.
 ///
 /// This interface is stricter and more structured, and therefore simpler than FRAME's.
+/// If you need to do something more powerful (which you probably don't)  and you
+/// understand exactly how Substrate's block authoring and Tuxedo's piece aggregation works
+/// (which you probably don't) you can directly implement the `InherentInternal` trait
+/// which is more powerful (and dangerous).
 pub trait TuxedoInherent<V, C: ConstraintChecker<V>>: Sized {
     type Error: Encode + IsFatalError;
 
@@ -116,7 +110,7 @@ pub trait TuxedoInherent<V, C: ConstraintChecker<V>>: Sized {
         previous_inherent: Option<(Transaction<V, C>, H256)>,
     ) -> Transaction<V, C>;
 
-    /// Perform off-chain pre-execution checks on the inherents.
+    /// Perform off-chain pre-execution checks on the inherent.
     /// The inherent data is supplied by the importing node.
     /// The inherent data available here is not guaranteed to be the
     /// same as what is available at authoring time.
@@ -131,6 +125,11 @@ pub trait TuxedoInherent<V, C: ConstraintChecker<V>>: Sized {
 /// exrinsics (as aggregate runtimes will need to) and removes the
 /// requirement that the generic outer constraint checker be buildable
 /// from `Self` so we can implement it for ().
+/// 
+/// If you are trying to implement some complex inherent logic that requires the interaction
+/// of multiple inherents, or features a variable number of inherents in each block, you can
+/// probably express it by implementing this trait, but such designs are probably too
+/// complicated. Think long and hard before implementing this trait directly.
 pub trait InherentInternal<V, C: ConstraintChecker<V>>: Sized {
     /// Create the inherent extrinsic to insert into a block that is being authored locally.
     /// The inherent data is supplied by the authoring node.
@@ -151,7 +150,7 @@ pub trait InherentInternal<V, C: ConstraintChecker<V>>: Sized {
 }
 
 /// An adapter to transform structured Tuxedo inherents into the more general and powerful
-/// InherentInternal trait. I really thought a blanket impl would work, but Rusts't type system is killing me.
+/// InherentInternal trait.
 #[derive(Debug, Default, TypeInfo, Clone, Copy)]
 pub struct TuxedoInherentAdapter<T>(T);
 
