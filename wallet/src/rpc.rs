d2a09e44e628cc30e865b649760ef337bf8ab85c
Joshy Orndorff <git-user-email.h0ly5@simplelogin.com>
2023-10-12 14:53:31 -0400

    make sure we get a proper non-opaque block in the wallet

--- wallet/src/rpc.rs
+++ wallet/src/rpc.rs
@@ -2,8 +2,9 @@
 //! RPC endpoint.
 
 use jsonrpsee::{core::client::ClientT, http_client::HttpClient, rpc_params};
-use runtime::Block;
+use runtime::{Transaction, Block, opaque::Block as OpaqueBlock};
 use sp_core::H256;
+use parity_scale_codec::{Encode, Decode};
 
 /// Typed helper to get the Node's block hash at a particular height
 pub async fn node_get_block_hash(height: u32, client: &HttpClient) -> anyhow::Result<Option<H256>> {
@@ -18,9 +19,19 @@ pub async fn node_get_block(hash: H256, client: &HttpClient) -> anyhow::Result<O
     let s = hex::encode(hash.0);
     let params = rpc_params![s];
 
-    let rpc_response: Option<serde_json::Value> = client.request("chain_getBlock", params).await?;
+    let maybe_rpc_response: Option<serde_json::Value> = client.request("chain_getBlock", params).await?;
+    let rpc_response = maybe_rpc_response.unwrap();
 
-    Ok(rpc_response
-        .and_then(|value| value.get("block").cloned())
-        .and_then(|maybe_block| serde_json::from_value(maybe_block).unwrap_or(None)))
+    let json_opaque_block = rpc_response.get("block").cloned().unwrap();
+    let opaque_block: OpaqueBlock = serde_json::from_value(json_opaque_block).unwrap();
+
+    // I need a structured blcok ,not an opaque one. To achieve that, I'll
+    // scale encode it, then once again decode it.
+    // Feels kind of like a hack, but I honestly don't know what else to do.
+    // I don't see any way to get the bytes out of an OpaqueExtrinsic.
+    let scale_bytes = opaque_block.encode();
+
+    let structured_block = Block::decode(&mut &scale_bytes[..]).unwrap();
+    
+    Ok(Some(structured_block))
 }
