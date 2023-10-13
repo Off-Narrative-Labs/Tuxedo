05851c2ce4e604e7a1b83306facf121919fc1f44
muraca <mmuraca247@gmail.com>
2023-10-13 16:29:01 +0200

    improved build_genesis_block
    Signed-off-by: muraca <mmuraca247@gmail.com>

--- node/src/tuxedo_genesis_builder.rs
+++ node/src/tuxedo_genesis_builder.rs
@@ -1,12 +1,13 @@
-/// Custom GenesisBlockBuilder for Tuxedo, to allow extrinsics to be added to the genesis block.
+//! Custom GenesisBlockBuilder for Tuxedo, to allow extrinsics to be added to the genesis block.
+
 use parity_scale_codec::Encode;
 use sc_chain_spec::BuildGenesisBlock;
-use sc_client_api::backend::Backend;
+use sc_client_api::backend::{Backend, BlockImportOperation};
 use sc_executor::RuntimeVersionOf;
 use sp_core::{storage::Storage, traits::CodeExecutor};
 use sp_runtime::{
     traits::{Block as BlockT, Hash as HashT, Header as HeaderT, Zero},
-    BuildStorage,
+    BuildStorage, StateVersion,
 };
 use std::sync::Arc;
 
@@ -44,22 +45,6 @@ impl<Block: BlockT, B: Backend<Block>, E: RuntimeVersionOf + CodeExecutor>
             extrinsics,
         })
     }
-
-    fn extrinsics_root(&self) -> sp_blockchain::Result<<Block as BlockT>::Hash> {
-        let state_version =
-            sc_chain_spec::resolve_state_version_from_wasm(&self.genesis_storage, &self.executor)?;
-
-        Ok(
-            <<<Block as BlockT>::Header as HeaderT>::Hashing as HashT>::ordered_trie_root(
-                self.extrinsics.iter().map(Encode::encode).collect(),
-                state_version,
-            ),
-        )
-    }
-
-    fn state_root(&self) -> Block::Hash {
-        todo!()
-    }
 }
 
 impl<Block: BlockT, B: Backend<Block>, E: RuntimeVersionOf + CodeExecutor> BuildGenesisBlock<Block>
@@ -68,17 +53,35 @@ impl<Block: BlockT, B: Backend<Block>, E: RuntimeVersionOf + CodeExecutor> Build
     type BlockImportOperation = <B as Backend<Block>>::BlockImportOperation;
 
     fn build_genesis_block(self) -> sp_blockchain::Result<(Block, Self::BlockImportOperation)> {
+        let state_version =
+            sc_chain_spec::resolve_state_version_from_wasm(&self.genesis_storage, &self.executor)?;
+
+        let extrinsics_root =
+            <<<Block as BlockT>::Header as HeaderT>::Hashing as HashT>::ordered_trie_root(
+                self.extrinsics.iter().map(Encode::encode).collect(),
+                state_version,
+            );
+
+        // TODO apply extrinsics
+
+        let mut op = self.backend.begin_operation()?;
+        let state_root = op.set_genesis_state(
+            self.genesis_storage,
+            self.commit_genesis_state,
+            state_version,
+        )?;
+
         let block = Block::new(
             HeaderT::new(
                 Zero::zero(),
-                self.extrinsics_root()?,
-                self.state_root(),
+                extrinsics_root,
+                state_root,
                 Default::default(),
                 Default::default(),
             ),
             self.extrinsics,
         );
 
-        todo!()
+        Ok((block, op))
     }
 }
