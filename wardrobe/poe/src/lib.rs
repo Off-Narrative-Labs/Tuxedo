8c09593bd7cbf03e3228dc00f4ff30379ddd6979
Joshy Orndorff <git-user-email.h0ly5@simplelogin.com>
2023-10-13 10:22:31 -0400

    remove unnecessary scale_info skip_type_params

--- wardrobe/poe/src/lib.rs
+++ wardrobe/poe/src/lib.rs
@@ -85,7 +85,6 @@ pub trait PoeConfig {
 /// waste of caller fees.
 #[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
 #[derive(Encode, Decode, DebugNoBound, CloneNoBound, PartialEq, Eq, TypeInfo)]
-#[scale_info(skip_type_params(T))]
 pub struct PoeClaim<T>(PhantomData<T>);
 
 impl<T: PoeConfig> SimpleConstraintChecker for PoeClaim<T> {
