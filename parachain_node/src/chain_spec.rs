cf004d48ec3f97328a122dece5c5681c490c7a0e
Joshy Orndorff <git-user-email.h0ly5@simplelogin.com>
2023-10-20 13:57:35 -0400

    change default paraid to 2000

--- parachain_node/src/chain_spec.rs
+++ parachain_node/src/chain_spec.rs
@@ -68,7 +68,7 @@ pub fn development_config() -> ChainSpec {
         None,
         Extensions {
             relay_chain: "rococo-local".into(), // You MUST set this to the correct network!
-            para_id: 1000,
+            para_id: 2000,
         },
     )
 }
@@ -100,7 +100,7 @@ pub fn local_testnet_config() -> ChainSpec {
         // Extensions
         Extensions {
             relay_chain: "rococo-local".into(), // You MUST set this to the correct network!
-            para_id: 1000,
+            para_id: 2000,
         },
     )
 }
