9be177da7cfec883195e7b7553f99a084ec74b76
Joshy Orndorff <git-user-email.h0ly5@simplelogin.com>
2023-10-24 15:59:37 -0400

    Mock parachain inherent in standalone node.
    Can't author block 2 now because it can't find parachain inherent in previous block.

--- node/src/service.rs
+++ node/src/service.rs
@@ -12,6 +12,9 @@ use sc_transaction_pool_api::OffchainTransactionPoolFactory;
 use sp_consensus_aura::sr25519::AuthorityPair as AuraPair;
 use std::{sync::Arc, time::Duration};
 
+#[cfg(feature = "mock-parachain")]
+use cumulus_primitives_parachain_inherent::MockValidationDataInherentDataProvider;
+
 // Our native executor instance.
 pub struct ExecutorDispatch;
 
@@ -251,17 +254,41 @@ pub fn new_full(config: Configuration) -> Result<TaskManager, ServiceError> {
                         let parent_block = maybe_parent_block?
                             .ok_or(sp_blockchain::Error::UnknownBlock(parent_hash.to_string()))?
                             .block;
+
+                        #[cfg(feature = "mock-parachain")]
+                        let mocked_parachain = {
+                            use sp_api::{BlockT, HeaderT};
+
+                            MockValidationDataInherentDataProvider {
+                                current_para_block: parent_block.header().number() + 1,
+                                relay_offset: 1000,
+                                relay_blocks_per_para_block: 2,
+                                para_blocks_per_relay_epoch: 10,
+                                relay_randomness_config: (),
+                                xcm_config: Default::default(),
+                                raw_downward_messages: Default::default(),
+                                raw_horizontal_messages: Default::default(),
+                            }
+                        };
+
                         let parent_idp =
                             tuxedo_core::inherents::ParentBlockInherentDataProvider(parent_block);
                         let timestamp = sp_timestamp::InherentDataProvider::from_system_time();
 
+                        //TODO Determine what to do about the slot inherent data provider. It is not included in the parachain template.
                         let slot =
                             sp_consensus_aura::inherents::InherentDataProvider::from_timestamp_and_slot_duration(
                                 *timestamp,
                                 slot_duration,
                             );
 
-                        Ok((slot, parent_idp, timestamp))
+                        Ok((
+                            slot,
+                            parent_idp,
+                            timestamp,
+                            #[cfg(feature = "mock-parachain")]
+                            mocked_parachain
+                        ))
                     }
                 },
                 force_authoring,
