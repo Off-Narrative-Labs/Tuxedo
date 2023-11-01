41a85711439cacd881499481abd96c99fd77aa5e
muraca <mmuraca247@gmail.com>
2023-11-01 16:22:19 +0100

    use externalities in parachain piece tests
    Signed-off-by: muraca <mmuraca247@gmail.com>

--- wardrobe/parachain/src/tests.rs
+++ wardrobe/parachain/src/tests.rs
@@ -38,104 +38,120 @@ fn new_data_from_relay_parent_number(relay_parent_number: u32) -> ParachainInher
 
 #[test]
 fn update_parachain_info_happy_path() {
-    let old: DynamicallyTypedData = new_data_from_relay_parent_number(3).into();
-    let inputs: Vec<Output<UpForGrabs>> = vec![old.into()];
-    let new: DynamicallyTypedData = new_data_from_relay_parent_number(4).into();
-    let outputs: Vec<Output<UpForGrabs>> = vec![new.into()];
+    sp_io::TestExternalities::new_empty().execute_with(|| {
+        let old: DynamicallyTypedData = new_data_from_relay_parent_number(3).into();
+        let inputs: Vec<Output<UpForGrabs>> = vec![old.into()];
+        let new: DynamicallyTypedData = new_data_from_relay_parent_number(4).into();
+        let outputs: Vec<Output<UpForGrabs>> = vec![new.into()];
 
-    assert_eq!(
-        SetParachainInfo::<AlwaysBlockTwo>(Default::default()).check(&inputs, &[], &outputs),
-        Ok(0),
-    );
+        assert_eq!(
+            SetParachainInfo::<AlwaysBlockTwo>(Default::default()).check(&inputs, &[], &outputs),
+            Ok(0),
+        );
+    });
 }
 
 #[test]
 fn update_parachain_info_relay_block_not_increasing() {
-    let old: DynamicallyTypedData = new_data_from_relay_parent_number(3).into();
-    let inputs: Vec<Output<UpForGrabs>> = vec![old.into()];
-    let new: DynamicallyTypedData = new_data_from_relay_parent_number(3).into();
-    let outputs: Vec<Output<UpForGrabs>> = vec![new.into()];
+    sp_io::TestExternalities::new_empty().execute_with(|| {
+        let old: DynamicallyTypedData = new_data_from_relay_parent_number(3).into();
+        let inputs: Vec<Output<UpForGrabs>> = vec![old.into()];
+        let new: DynamicallyTypedData = new_data_from_relay_parent_number(3).into();
+        let outputs: Vec<Output<UpForGrabs>> = vec![new.into()];
 
-    assert_eq!(
-        SetParachainInfo::<AlwaysBlockTwo>(Default::default()).check(&inputs, &[], &outputs),
-        Err(RelayBlockNotIncreasing),
-    );
+        assert_eq!(
+            SetParachainInfo::<AlwaysBlockTwo>(Default::default()).check(&inputs, &[], &outputs),
+            Err(RelayBlockNotIncreasing),
+        );
+    });
 }
 
 #[test]
 fn update_parachain_info_extra_inputs() {
-    let old1: DynamicallyTypedData = new_data_from_relay_parent_number(3).into();
-    let old2: DynamicallyTypedData = Bogus.into();
-    let inputs: Vec<Output<UpForGrabs>> = vec![old1.into(), old2.into()];
-    let new: DynamicallyTypedData = new_data_from_relay_parent_number(4).into();
-    let outputs: Vec<Output<UpForGrabs>> = vec![new.into()];
+    sp_io::TestExternalities::new_empty().execute_with(|| {
+        let old1: DynamicallyTypedData = new_data_from_relay_parent_number(3).into();
+        let old2: DynamicallyTypedData = Bogus.into();
+        let inputs: Vec<Output<UpForGrabs>> = vec![old1.into(), old2.into()];
+        let new: DynamicallyTypedData = new_data_from_relay_parent_number(4).into();
+        let outputs: Vec<Output<UpForGrabs>> = vec![new.into()];
 
-    assert_eq!(
-        SetParachainInfo::<AlwaysBlockTwo>(Default::default()).check(&inputs, &[], &outputs),
-        Err(ExtraInputs)
-    );
+        assert_eq!(
+            SetParachainInfo::<AlwaysBlockTwo>(Default::default()).check(&inputs, &[], &outputs),
+            Err(ExtraInputs)
+        );
+    });
 }
 
 #[test]
 fn update_parachain_info_missing_input() {
-    let inputs: Vec<Output<UpForGrabs>> = vec![];
-    let new: DynamicallyTypedData = new_data_from_relay_parent_number(4).into();
-    let outputs: Vec<Output<UpForGrabs>> = vec![new.into()];
+    sp_io::TestExternalities::new_empty().execute_with(|| {
+        let inputs: Vec<Output<UpForGrabs>> = vec![];
+        let new: DynamicallyTypedData = new_data_from_relay_parent_number(4).into();
+        let outputs: Vec<Output<UpForGrabs>> = vec![new.into()];
 
-    assert_eq!(
-        SetParachainInfo::<AlwaysBlockTwo>(Default::default()).check(&inputs, &[], &outputs),
-        Err(MissingPreviousInfo)
-    );
+        assert_eq!(
+            SetParachainInfo::<AlwaysBlockTwo>(Default::default()).check(&inputs, &[], &outputs),
+            Err(MissingPreviousInfo)
+        );
+    });
 }
 
 #[test]
 fn update_parachain_info_bogus_input() {
-    let old: DynamicallyTypedData = Bogus.into();
-    let inputs: Vec<Output<UpForGrabs>> = vec![old.into()];
-    let new: DynamicallyTypedData = new_data_from_relay_parent_number(3).into();
-    let outputs: Vec<Output<UpForGrabs>> = vec![new.into()];
+    sp_io::TestExternalities::new_empty().execute_with(|| {
+        let old: DynamicallyTypedData = Bogus.into();
+        let inputs: Vec<Output<UpForGrabs>> = vec![old.into()];
+        let new: DynamicallyTypedData = new_data_from_relay_parent_number(3).into();
+        let outputs: Vec<Output<UpForGrabs>> = vec![new.into()];
 
-    assert_eq!(
-        SetParachainInfo::<AlwaysBlockTwo>(Default::default()).check(&inputs, &[], &outputs),
-        Err(BadlyTyped)
-    );
+        assert_eq!(
+            SetParachainInfo::<AlwaysBlockTwo>(Default::default()).check(&inputs, &[], &outputs),
+            Err(BadlyTyped)
+        );
+    });
 }
 
 #[test]
 fn update_parachain_info_extra_outputs() {
-    let old: DynamicallyTypedData = new_data_from_relay_parent_number(3).into();
-    let inputs: Vec<Output<UpForGrabs>> = vec![old.into()];
-    let new1: DynamicallyTypedData = new_data_from_relay_parent_number(4).into();
-    let new2: DynamicallyTypedData = Bogus.into();
-    let outputs: Vec<Output<UpForGrabs>> = vec![new1.into(), new2.into()];
+    sp_io::TestExternalities::new_empty().execute_with(|| {
+        let old: DynamicallyTypedData = new_data_from_relay_parent_number(3).into();
+        let inputs: Vec<Output<UpForGrabs>> = vec![old.into()];
+        let new1: DynamicallyTypedData = new_data_from_relay_parent_number(4).into();
+        let new2: DynamicallyTypedData = Bogus.into();
+        let outputs: Vec<Output<UpForGrabs>> = vec![new1.into(), new2.into()];
 
-    assert_eq!(
-        SetParachainInfo::<AlwaysBlockTwo>(Default::default()).check(&inputs, &[], &outputs),
-        Err(ExtraOutputs)
-    );
+        assert_eq!(
+            SetParachainInfo::<AlwaysBlockTwo>(Default::default()).check(&inputs, &[], &outputs),
+            Err(ExtraOutputs)
+        );
+    });
 }
 
 #[test]
 fn update_parachain_info_missing_output() {
-    let old: DynamicallyTypedData = new_data_from_relay_parent_number(3).into();
-    let inputs: Vec<Output<UpForGrabs>> = vec![old.into()];
-    let outputs: Vec<Output<UpForGrabs>> = vec![];
+    sp_io::TestExternalities::new_empty().execute_with(|| {
+        let old: DynamicallyTypedData = new_data_from_relay_parent_number(3).into();
+        let inputs: Vec<Output<UpForGrabs>> = vec![old.into()];
+        let outputs: Vec<Output<UpForGrabs>> = vec![];
 
-    assert_eq!(
-        SetParachainInfo::<AlwaysBlockTwo>(Default::default()).check(&inputs, &[], &outputs),
-        Err(MissingNewInfo)
-    );
+        assert_eq!(
+            SetParachainInfo::<AlwaysBlockTwo>(Default::default()).check(&inputs, &[], &outputs),
+            Err(MissingNewInfo)
+        );
+    });
 }
 
 #[test]
 fn update_parachain_info_bogus_output() {
-    let old: DynamicallyTypedData = new_data_from_relay_parent_number(3).into();
-    let inputs: Vec<Output<UpForGrabs>> = vec![old.into()];
-    let new: DynamicallyTypedData = Bogus.into();
-    let outputs: Vec<Output<UpForGrabs>> = vec![new.into()];
+    sp_io::TestExternalities::new_empty().execute_with(|| {
+        let old: DynamicallyTypedData = new_data_from_relay_parent_number(3).into();
+        let inputs: Vec<Output<UpForGrabs>> = vec![old.into()];
+        let new: DynamicallyTypedData = Bogus.into();
+        let outputs: Vec<Output<UpForGrabs>> = vec![new.into()];
 
-    assert_eq!(
-        SetParachainInfo::<AlwaysBlockTwo>(Default::default()).check(&inputs, &[], &outputs),
-        Err(BadlyTyped)
-    );
+        assert_eq!(
+            SetParachainInfo::<AlwaysBlockTwo>(Default::default()).check(&inputs, &[], &outputs),
+            Err(BadlyTyped)
+        );
+    });
 }
