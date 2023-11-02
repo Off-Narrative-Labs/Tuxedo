e0f597cff1406d51ba290e4a3e0ec63fe1530876
muraca <mmuraca247@gmail.com>
2023-11-02 10:03:08 +0100

    improve wallet cli doc comments
    Signed-off-by: muraca <mmuraca247@gmail.com>

--- wallet/src/cli.rs
+++ wallet/src/cli.rs
@@ -18,16 +18,14 @@ pub struct Cli {
     /// RPC endpoint of the node that this wallet will connect to
     pub endpoint: String,
 
-    #[arg(long, short)]
-    /// Path where the wallet data is stored. Wallet data is just keystore at the moment,
-    /// but will contain a local database of UTXOs in the future.
-    ///
-    /// Default value is platform specific
+    #[arg(long, short, verbatim_doc_comment)]
+    /// Path where the wallet data is stored.
+    /// Wallet data is just keystore at the moment, but will contain a local database of UTXOs in the future.
+    /// Default value is platform specific.
     pub data_path: Option<PathBuf>,
 
-    #[arg(long)]
+    #[arg(long, verbatim_doc_comment)]
     /// Skip the initial sync that the wallet typically performs with the node.
-    ///
     /// The wallet will use the latest data it had previously synced.
     pub no_sync: bool,
 
@@ -42,8 +40,8 @@ pub enum Command {
     AmoebaDemo,
 
     /// Verify that a particular coin exists.
-    ///
     /// Show its value and owner from both chain storage and the local database.
+    #[command(verbatim_doc_comment)]
     VerifyCoin {
         /// A hex-encoded output reference
         #[arg(value_parser = output_ref_from_string)]
@@ -51,8 +49,9 @@ pub enum Command {
     },
 
     /// Spend some coins.
-    ///
-    /// For now, all outputs in a single transaction go to the same recipient. FixMe: #62
+    /// For now, all outputs in a single transaction go to the same recipient.
+    // FixMe: #62
+    #[command(verbatim_doc_comment)]
     SpendCoins(SpendArgs),
 
     /// Insert a private key into the keystore to later use when signing transactions.
@@ -65,7 +64,7 @@ pub enum Command {
         // sync_height: Option<u32>,
     },
 
-    /// Generate a private key using either some or no password and insert into the keystore
+    /// Generate a private key using either some or no password and insert into the keystore.
     GenerateKey {
         /// Initialize a public/private key pair with a password
         password: Option<String>,
@@ -75,16 +74,18 @@ pub enum Command {
     ShowKeys,
 
     /// Remove a specific key from the keystore.
-    /// WARNING! This will permanently delete the private key information. Make sure your
-    /// keys are backed up somewhere safe.
+    /// WARNING! This will permanently delete the private key information.
+    /// Make sure your keys are backed up somewhere safe.
+    #[command(verbatim_doc_comment)]
     RemoveKey {
         /// The public key to remove
         #[arg(value_parser = h256_from_string)]
         pub_key: H256,
     },
 
-    /// For each key tracked by the wallet, shows the sum of all UTXO values
-    /// owned by that key. This sum is sometimes known as the "balance".
+    /// For each key tracked by the wallet, shows the sum of all UTXO values owned by that key.
+    /// This sum is sometimes known as the "balance".
+    #[command(verbatim_doc_comment)]
     ShowBalance,
 
     /// Show the complete list of UTXOs known to the wallet.
@@ -95,7 +96,7 @@ pub enum Command {
 pub struct SpendArgs {
     /// An input to be consumed by this transaction. This argument may be specified multiple times.
     /// They must all be coins.
-    #[arg(long, short, value_parser = output_ref_from_string)]
+    #[arg(long, short, verbatim_doc_comment, value_parser = output_ref_from_string)]
     pub input: Vec<OutputRef>,
 
     // /// All inputs to the transaction will be from this same sender.
@@ -105,14 +106,13 @@ pub struct SpendArgs {
 
     // https://docs.rs/clap/latest/clap/_derive/_cookbook/typed_derive/index.html
     // shows how to specify a custom parsing function
-    /// Hex encoded address (sr25519 pubkey) of the recipient
-    #[arg(long, short, value_parser = h256_from_string, default_value = SHAWN_PUB_KEY)]
+    /// Hex encoded address (sr25519 pubkey) of the recipient.
+    #[arg(long, short, verbatim_doc_comment, value_parser = h256_from_string, default_value = SHAWN_PUB_KEY)]
     pub recipient: H256,
 
     // The `action = Append` allows us to accept the same value multiple times.
-    /// An output amount. For the transaction to be valid, the outputs must add up to less than
-    /// the sum of the inputs. The wallet will not enforce this and will gladly send an invalid transaction
-    /// which will then e rejected by the node.
-    #[arg(long, short, action = Append)]
+    /// An output amount. For the transaction to be valid, the outputs must add up to less than the sum of the inputs.
+    /// The wallet will not enforce this and will gladly send an invalid which will then be rejected by the node.
+    #[arg(long, short, verbatim_doc_comment, action = Append)]
     pub output_amount: Vec<u128>,
 }
