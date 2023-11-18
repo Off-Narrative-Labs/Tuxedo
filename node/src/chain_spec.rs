4df7ea3ae74a31f03450c998294aa071a5c67c6a
Joshy Orndorff <git-user-email.h0ly5@simplelogin.com>
2023-11-17 23:37:18 -0500

    make chain spec much more minimal and rely on default runtime

--- node/src/chain_spec.rs
+++ node/src/chain_spec.rs
@@ -7,29 +7,6 @@ use sc_service::ChainType;
 /// Specialized `ChainSpec`. This is a specialization of the general Substrate ChainSpec type.
 pub type ChainSpec = sc_service::GenericChainSpec<RuntimeGenesisConfig>;
 
-// /// Generate a crypto pair from seed.
-// pub fn get_from_seed<TPublic: Public>(seed: &str) -> <TPublic::Pair as Pair>::Public {
-// 	TPublic::Pair::from_string(&format!("//{}", seed), None)
-// 		.expect("static values are valid; qed")
-// 		.public()
-// }
-
-// type AccountPublic = <Signature as Verify>::Signer;
-
-// /// Generate an account ID from seed.
-// pub fn get_account_id_from_seed<TPublic: Public>(seed: &str) -> AccountId
-// where
-// 	AccountPublic: From<<TPublic::Pair as Pair>::Public>,
-// {
-// 	AccountPublic::from(get_from_seed::<TPublic>(seed)).into_account()
-// }
-
-// /// Generate an Aura authority key.
-// pub fn authority_keys_from_seed(s: &str) -> (AuraId, GrandpaId) {
-// 	(get_from_seed::<AuraId>(s), get_from_seed::<GrandpaId>(s))
-// }
-
-//TODO try the new builder pattern. I just wasn't sure about this patch thing.
 pub fn development_config() -> Result<ChainSpec, String> {
     Ok(ChainSpec::builder(
         WASM_BINARY.ok_or_else(|| "Development wasm not available".to_string())?,
@@ -38,7 +15,6 @@ pub fn development_config() -> Result<ChainSpec, String> {
     .with_name("Development")
     .with_id("dev")
     .with_chain_type(ChainType::Development)
-    .with_genesis_config_patch(development_genesis_config())
     .build())
 }
 
@@ -50,6 +26,5 @@ pub fn local_testnet_config() -> Result<ChainSpec, String> {
     .with_name("Local Testnet")
     .with_id("local_testnet")
     .with_chain_type(ChainType::Local)
-    .with_genesis_config_patch(development_genesis_config())
     .build())
 }
