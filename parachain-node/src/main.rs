0bdb83ff03dd7bc6bd67d0e8ad1f7f5b8204b5bf
Joshy Orndorff <git-user-email.h0ly5@simplelogin.com>
2023-11-22 14:59:31 -0500

    Bring in dev service

--- parachain-node/src/main.rs
+++ parachain-node/src/main.rs
@@ -3,11 +3,11 @@
 #![warn(missing_docs)]
 
 mod chain_spec;
-#[macro_use]
-mod service;
 mod cli;
 mod command;
+mod dev_service;
 mod rpc;
+mod service;
 
 fn main() -> sc_cli::Result<()> {
     command::run()
