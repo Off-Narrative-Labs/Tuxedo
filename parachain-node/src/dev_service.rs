c0a63f911d8a0bb8538888acf4a8071db6df6f9a
Joshy Orndorff <git-user-email.h0ly5@simplelogin.com>
2023-11-27 14:24:13 -0500

    try more isolated dev service
    I found this laying arou9nd dirty on my disk and checking it in.

--- parachain-node/src/dev_service.rs
+++ parachain-node/src/dev_service.rs
@@ -2,24 +2,123 @@
 //! but without a backing relay chain. This allows developers to quickly and easily spin up parachain
 //! nodes using the --dev flag, for example. It can also be used in integration tests.
 
-// std
-use std::time::Duration;
+use parachain_template_runtime::{self, opaque::Block, RuntimeApi};
+use sc_client_api::BlockBackend;
+use sc_consensus_manual_seal::consensus::aura::AuraConsensusDataProvider;
+pub use sc_executor::NativeElseWasmExecutor;
+use sc_service::{error::Error as ServiceError, Configuration, TaskManager};
+use sc_telemetry::{Telemetry, TelemetryWorker};
+use std::{sync::Arc, time::Duration};
+use tuxedo_core::genesis::TuxedoGenesisBlockBuilder;
 
-// Cumulus Imports
 use cumulus_primitives_parachain_inherent::MockValidationDataInherentDataProvider;
 
-// Substrate Imports
-use crate::service::new_partial;
-use sc_client_api::HeaderBackend;
-use sc_consensus_manual_seal::consensus::aura::AuraConsensusDataProvider;
-use sc_service::{Configuration, Error as ServiceError, TaskManager};
+// Our native executor instance.
+pub struct ExecutorDispatch;
+
+impl sc_executor::NativeExecutionDispatch for ExecutorDispatch {
+    type ExtendHostFunctions = ();
+
+    fn dispatch(method: &str, data: &[u8]) -> Option<Vec<u8>> {
+        parachain_template_runtime::api::dispatch(method, data)
+    }
+
+    fn native_version() -> sc_executor::NativeVersion {
+        parachain_template_runtime::native_version()
+    }
+}
+
+pub(crate) type FullClient =
+    sc_service::TFullClient<Block, RuntimeApi, NativeElseWasmExecutor<ExecutorDispatch>>;
+type FullBackend = sc_service::TFullBackend<Block>;
+type FullSelectChain = sc_consensus::LongestChain<FullBackend, Block>;
+
+
+#[allow(clippy::type_complexity)]
+pub fn new_partial(
+    config: &Configuration,
+) -> Result<
+    sc_service::PartialComponents<
+        FullClient,
+        FullBackend,
+        FullSelectChain,
+        sc_consensus::DefaultImportQueue<Block>,
+        sc_transaction_pool::FullPool<Block, FullClient>,
+        (Option<Telemetry>),
+    >,
+    ServiceError,
+> {
+    let telemetry = config
+        .telemetry_endpoints
+        .clone()
+        .filter(|x| !x.is_empty())
+        .map(|endpoints| -> Result<_, sc_telemetry::Error> {
+            let worker = TelemetryWorker::new(16)?;
+            let telemetry = worker.handle().new_telemetry(endpoints);
+            Ok((worker, telemetry))
+        })
+        .transpose()?;
+
+    let executor = sc_service::new_native_or_wasm_executor(config);
+
+    let backend = sc_service::new_db_backend(config.db_config())?;
+    let genesis_block_builder = TuxedoGenesisBlockBuilder::new(
+        config.chain_spec.as_storage_builder(),
+        !config.no_genesis(),
+        backend.clone(),
+        executor.clone(),
+    )?;
+
+    let (client, backend, keystore_container, task_manager) =
+        sc_service::new_full_parts_with_genesis_builder::<Block, RuntimeApi, _, _>(
+            config,
+            telemetry.as_ref().map(|(_, telemetry)| telemetry.handle()),
+            executor,
+            backend,
+            genesis_block_builder,
+        )?;
+    let client = Arc::new(client);
+
+    let telemetry = telemetry.map(|(worker, telemetry)| {
+        task_manager
+            .spawn_handle()
+            .spawn("telemetry", None, worker.run());
+        telemetry
+    });
+
+    let select_chain = sc_consensus::LongestChain::new(backend.clone());
+
+    let transaction_pool = sc_transaction_pool::BasicPool::new_full(
+        config.transaction_pool.clone(),
+        config.role.is_authority().into(),
+        config.prometheus_registry(),
+        task_manager.spawn_essential_handle(),
+        client.clone(),
+    );
+
+    let import_queue = sc_consensus_manual_seal::import_queue(
+		Box::new(client.clone()),
+		&task_manager.spawn_essential_handle(),
+		config.prometheus_registry(),
+	);
+
+    Ok(sc_service::PartialComponents {
+        client,
+        backend,
+        task_manager,
+        import_queue,
+        keystore_container,
+        select_chain,
+        transaction_pool,
+        other: (telemetry),
+    })
+}
 
 /// Builds a new development service. This service uses manual seal, and mocks
 /// the parachain inherent.
 pub fn new_dev(mut config: Configuration) -> Result<TaskManager, ServiceError> {
     use async_io::Timer;
     use sc_consensus_manual_seal::{run_manual_seal, EngineCommand, ManualSealParams};
-    use sp_core::H256;
 
     let sc_service::PartialComponents {
         client,
@@ -29,7 +128,7 @@ pub fn new_dev(mut config: Configuration) -> Result<TaskManager, ServiceError> {
         keystore_container,
         select_chain: _,
         transaction_pool,
-        other: (_block_import, telemetry, _telemetry_worker_handle),
+        other: (telemetry),
     } = new_partial(&mut config)?;
 
     // We don't use the block import or import queue provided from new_partial
@@ -84,7 +183,7 @@ pub fn new_dev(mut config: Configuration) -> Result<TaskManager, ServiceError> {
 
         let select_chain = sc_consensus::LongestChain::new(backend.clone());
 
-        let client_set_aside_for_cidp = client.clone();
+        let client_for_cidp = client.clone();
 
         // Create channels for mocked XCM messages.
         // let (downward_xcm_sender, downward_xcm_receiver) = flume::bounded::<Vec<u8>>(100);
@@ -104,31 +203,38 @@ pub fn new_dev(mut config: Configuration) -> Result<TaskManager, ServiceError> {
                 consensus_data_provider: Some(Box::new(AuraConsensusDataProvider::new(
                     client.clone(),
                 ))),
-                create_inherent_data_providers: move |block: H256, ()| {
-                    let current_para_block = client_set_aside_for_cidp
-                        .number(block)
-                        .expect("Header lookup should succeed")
-                        .expect("Header passed in as parent should be present in backend.");
+                create_inherent_data_providers: move |parent_hash, ()| {
+                    let maybe_parent_block = client_for_cidp.clone().block(parent_hash);
 
-                    // let downward_xcm_receiver = downward_xcm_receiver.clone();
-                    // let hrmp_xcm_receiver = hrmp_xcm_receiver.clone();
-
-                    // let client_for_xcm = client_set_aside_for_cidp.clone();
                     async move {
-                        let timestamp = sp_timestamp::InherentDataProvider::from_system_time();
+                        let parent_block = maybe_parent_block?
+                            .ok_or(sp_blockchain::Error::UnknownBlock(parent_hash.to_string()))?
+                            .block;
 
-                        let mocked_parachain = MockValidationDataInherentDataProvider {
-                            current_para_block,
-                            relay_offset: 1000,
-                            relay_blocks_per_para_block: 2,
-                            para_blocks_per_relay_epoch: 10,
-                            relay_randomness_config: (),
-                            xcm_config: Default::default(),
-                            raw_downward_messages: Default::default(),
-                            raw_horizontal_messages: Default::default(),
+                        let mocked_parachain = {
+                            use sp_api::{BlockT, HeaderT};
+
+                            MockValidationDataInherentDataProvider {
+                                current_para_block: parent_block.header().number() + 1,
+                                relay_offset: 1000,
+                                relay_blocks_per_para_block: 2,
+                                para_blocks_per_relay_epoch: 10,
+                                relay_randomness_config: (),
+                                xcm_config: Default::default(),
+                                raw_downward_messages: Default::default(),
+                                raw_horizontal_messages: Default::default(),
+                            }
                         };
 
-                        Ok((timestamp, mocked_parachain))
+                        let parent_idp =
+                            tuxedo_core::inherents::ParentBlockInherentDataProvider(parent_block);
+                        let timestamp = sp_timestamp::InherentDataProvider::from_system_time();
+
+                        Ok((
+                            parent_idp,
+                            timestamp,
+                            mocked_parachain,
+                        ))
                     }
                 },
             }),
