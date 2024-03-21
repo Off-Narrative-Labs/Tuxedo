f9d3ab2e8f79f5b1c82e0f609685325153cd0e68
Joshy Orndorff <admin@joshyorndorff.com>
2024-03-21 15:30:29 -0400

    Make more wallet stuff generic. Not sure this is the way...

--- wallet/src/rpc.rs
+++ wallet/src/rpc.rs
@@ -5,11 +5,11 @@ use crate::strip_0x_prefix;
 use anyhow::anyhow;
 use jsonrpsee::{core::client::ClientT, http_client::HttpClient, rpc_params};
 use parity_scale_codec::{Decode, Encode};
-use runtime::{opaque::Block as OpaqueBlock, Block};
+use runtime::{opaque::Block as OpaqueBlock};
 use sp_core::H256;
+use sp_runtime::generic::Block;
 use tuxedo_core::{
-    types::{Output, OutputRef},
-    Verifier,
+    types::{Output, OutputRef, Transaction}, ConstraintChecker, Verifier
 };
 
 /// Typed helper to get the Node's block hash at a particular height
@@ -21,7 +21,7 @@ pub async fn node_get_block_hash(height: u32, client: &HttpClient) -> anyhow::Re
 }
 
 /// Typed helper to get the node's full block at a particular hash
-pub async fn node_get_block(hash: H256, client: &HttpClient) -> anyhow::Result<Option<Block>> {
+pub async fn node_get_block<Block: Decode>(hash: H256, client: &HttpClient) -> anyhow::Result<Option<Block>> {
     let s = hex::encode(hash.0);
     let params = rpc_params![s];
 
