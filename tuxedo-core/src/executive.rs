9e2c908d67d3cefcd0db081a5b6f42f6fbf45405
Amit Nadiger <amith.nadiger@gmail.com>
2024-04-01 21:43:03 +0530

    Revert "web service implementation"
    This reverts commit 9c4f9030b7910c77ed08a5466fdca3ac487bc577.

--- tuxedo-core/src/executive.rs
+++ tuxedo-core/src/executive.rs
@@ -45,7 +45,7 @@ impl<B: BlockT<Extrinsic = Transaction<V, C>>, V: Verifier, C: ConstraintChecker
     pub fn validate_tuxedo_transaction(
         transaction: &Transaction<V, C>,
     ) -> Result<ValidTransaction, UtxoError<C::Error>> {
-        log::info!(
+        debug!(
             target: LOG_TARGET,
             "validating tuxedo transaction",
         );
@@ -54,11 +54,6 @@ impl<B: BlockT<Extrinsic = Transaction<V, C>>, V: Verifier, C: ConstraintChecker
         // Duplicate peeks are allowed, although they are inefficient and wallets should not create such transactions
         {
             let input_set: BTreeSet<_> = transaction.inputs.iter().map(|o| o.encode()).collect();
-            log::info!(
-                target: LOG_TARGET,
-                "input_set.len() {} and transaction.inputs.len()  {}",input_set.len(),
-                transaction.inputs.len()
-            );
             ensure!(
                 input_set.len() == transaction.inputs.len(),
                 UtxoError::DuplicateInput
@@ -112,7 +107,7 @@ impl<B: BlockT<Extrinsic = Transaction<V, C>>, V: Verifier, C: ConstraintChecker
                 index: index as u32,
             };
 
-            log::info!(
+            debug!(
                 target: LOG_TARGET,
                 "Checking for pre-existing output {:?}", output_ref
             );
@@ -138,7 +133,7 @@ impl<B: BlockT<Extrinsic = Transaction<V, C>>, V: Verifier, C: ConstraintChecker
         // If any of the inputs are missing, we cannot make any more progress
         // If they are all present, we may proceed to call the constraint checker
         if !missing_inputs.is_empty() {
-            log::info!(
+            debug!(
                 target: LOG_TARGET,
                 "Transaction is valid but still has missing inputs. Returning early.",
             );
